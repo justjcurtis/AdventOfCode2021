cfd9970058f06e086c54b2a1413145266c9633be
justjcurtis <justjcurtis@gmail.com>
2021-12-03 18:04:33 +0000

    test: Day 2 part2Handler

--- 2/test.js
+++ 2/test.js
@@ -1,4 +1,4 @@
-const { part1Handler } = require('./solution')
+const { part1Handler, part2Handler } = require('./solution')
 const { input } = require('./testData')
 
 describe('Day 2', () => {
@@ -42,6 +42,50 @@ describe('Day 2', () => {
             const result = part1Handler(cmd, x, { a, d, h })
             expect(result.a).toBe(a)
             expect(result.d).toBe(d - x)
+            expect(result.h).toBe(h)
+        })
+    })
+
+    describe('part2Handler', () => {
+        const forwardPart2TestData = [
+            ['f', 1, { a: 11, d: 12, h: 13 }],
+            ['f', 2, { a: 11, d: 12, h: 13 }],
+            ['f', 7, { a: 11, d: 12, h: 13 }],
+            ['f', 99, { a: 11, d: 12, h: 13 }],
+            ['f', -5, { a: 11, d: 12, h: 13 }],
+        ]
+        test.each(forwardPart2TestData)('for %p command part2Handler should add %p to h and add a * x to d', (cmd, x, { a, d, h }) => {
+            const result = part2Handler(cmd, x, { a, d, h })
+            expect(result.a).toBe(a)
+            expect(result.d).toBe(d + a * x)
+            expect(result.h).toBe(h + x)
+        })
+
+        const downPart2TestData = [
+            ['d', 1, { a: 11, d: 12, h: 13 }],
+            ['d', 2, { a: 11, d: 12, h: 13 }],
+            ['d', 7, { a: 11, d: 12, h: 13 }],
+            ['d', 99, { a: 11, d: 12, h: 13 }],
+            ['d', -5, { a: 11, d: 12, h: 13 }],
+        ]
+        test.each(downPart2TestData)('for %p command part2Handler should add %p to a', (cmd, x, { a, d, h }) => {
+            const result = part2Handler(cmd, x, { a, d, h })
+            expect(result.a).toBe(a + x)
+            expect(result.d).toBe(d)
+            expect(result.h).toBe(h)
+        })
+
+        const upPart2TestData = [
+            ['u', 1, { a: 11, d: 12, h: 13 }],
+            ['u', 2, { a: 11, d: 12, h: 13 }],
+            ['u', 7, { a: 11, d: 12, h: 13 }],
+            ['u', 99, { a: 11, d: 12, h: 13 }],
+            ['u', -5, { a: 11, d: 12, h: 13 }],
+        ]
+        test.each(upPart2TestData)('for %p command part2Handler should subtract %p from a', (cmd, x, { a, d, h }) => {
+            const result = part2Handler(cmd, x, { a, d, h })
+            expect(result.a).toBe(a - x)
+            expect(result.d).toBe(d)
             expect(result.h).toBe(h)
         })
     })
