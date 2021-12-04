a96902b38e2e3c2b9e130a931398114798050964
justjcurtis <justjcurtis@gmail.com>
2021-12-04 07:48:41 +0000

    feat: add Day 4 to run

--- run.js
+++ run.js
@@ -1,9 +1,11 @@
 const day1Input = require('./1/input')
 const day2Input = require('./2/input')
 const day3Input = require('./3/input')
+const day4Input = require('./4/input')
 const day1 = require('./1/solution')
 const day2 = require('./2/solution')
 const day3 = require('./3/solution')
+const day4 = require('./4/solution')
 
 console.log("--- Day 1 ---")
 console.log("Part 1:", day1.solution(day1Input)) // 1602
@@ -18,4 +20,10 @@ console.log()
 console.log("--- Day 3 ---")
 console.log('Part 1:', day3.solution1(day3Input)) // 2498354
 console.log('Part 2:', day3.solution2(day3Input)) // 3277956
+console.log('-------------')
+console.log('-------------')
+console.log()
+console.log("--- Day 4 ---")
+console.log('Part 1:', day4.getFirstBoardToWin(day4Input.boards, day4Input.drawings)) // 63552
+console.log('Part 2:', day4.getLastBoardToWin(day4Input.boards, day4Input.drawings)) // 9020
 console.log('-------------')
