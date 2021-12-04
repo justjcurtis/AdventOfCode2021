05cedb6b7797305a54167e53a4681ae9ada1030b
justjcurtis <justjcurtis@gmail.com>
2021-12-04 07:45:56 +0000

    test: Day 4

---
+++ 4/test.js
@@ -0,0 +1,83 @@
+const { boards, drawings, expectedCompletions, nonWinBoards, winBoards } = require('./testData')
+const { checkWin, completeBoard, getBlankMarkings, getFirstBoardToWin, getLastBoardToWin, getScore } = require('./solution')
+
+describe('Day 4', () => {
+    describe('checkWin', () => {
+        test.each(winBoards)('should return true for winning board', (markings, i, j) => {
+            const result = checkWin(markings, i, j)
+            expect(result).toBe(true)
+        })
+        test.each(nonWinBoards)('should return false for non winning board', (markings) => {
+            const x = markings[0].length
+            const y = markings.length
+            for (let i = 0; i < x; i++) {
+                for (let j = 0; j < y; j++) {
+                    const result = checkWin(markings, i, j)
+                    expect(result).toBe(false)
+                }
+            }
+        })
+    })
+    describe('completeBoard', () => {
+        const completeTestData = [
+            [1, expectedCompletions[0], boards[0]],
+            [2, expectedCompletions[1], boards[1]],
+            [3, expectedCompletions[2], boards[2]],
+        ]
+        test.each(completeTestData)('given board %p from test data should return %p', (_, expected, board) => {
+            const result = completeBoard(board, drawings)
+            expect(result).toStrictEqual(expected)
+        })
+    })
+    describe('getBlankMarkings', () => {
+        const markingsTestData = [
+            [5, 5],
+            [1, 15],
+            [13, 22],
+            [101, 123],
+        ]
+        test.each(markingsTestData)('should have size corresponding to input %p,%p and all values false', (i, j) => {
+            const result = getBlankMarkings(i, j)
+            expect(result.length).toBe(i)
+            for (let a = 0; a < i; a++) {
+                expect(result[a].length).toBe(j)
+                for (let b = 0; b < j; b++) {
+                    expect(result[a][b]).toBe(false)
+                }
+            }
+        })
+        test('should have default size 5,5 given no input and all values false', () => {
+            const result = getBlankMarkings()
+            expect(result.length).toBe(5)
+            for (let a = 0; a < 5; a++) {
+                expect(result[a].length).toBe(5)
+                for (let b = 0; b < 5; b++) {
+                    expect(result[a][b]).toBe(false)
+                }
+            }
+        })
+    })
+    describe('getFirstBoardToWin', () => {
+        test('should return expected first board to win hash', () => {
+            const result = getFirstBoardToWin(boards, drawings)
+            expect(result).toBe(4512)
+        })
+    })
+    describe('getLastBoardToWin', () => {
+        test('should return expected last board to win hash', () => {
+            const result = getLastBoardToWin(boards, drawings)
+            expect(result).toBe(1924)
+        })
+    })
+    describe('getScore', () => {
+        const scoreTestData = [
+            [1, 2192, completeBoard(boards[0], drawings)],
+            [2, 1924, completeBoard(boards[1], drawings)],
+            [3, 4512, completeBoard(boards[2], drawings)],
+        ]
+        test.each(scoreTestData)('given board %p from test data, should return %p', (_, expected, win) => {
+            const result = getScore(win)
+            expect(result).toBe(expected)
+        })
+    })
+})
---
+++ 4/testData.js
@@ -0,0 +1,133 @@
+const drawings = [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1]
+const boards = [
+    [[22, 13, 17, 11, 0],
+    [8, 2, 23, 4, 24],
+    [21, 9, 14, 16, 7],
+    [6, 10, 3, 18, 5],
+    [1, 12, 20, 15, 19]],
+
+    [[3, 15, 0, 2, 22],
+    [9, 18, 13, 17, 5],
+    [19, 8, 7, 25, 23],
+    [20, 11, 10, 24, 4],
+    [14, 21, 16, 12, 6]],
+
+    [[14, 21, 17, 24, 4],
+    [10, 16, 15, 9, 19],
+    [18, 8, 23, 26, 20],
+    [22, 11, 13, 6, 5],
+    [2, 0, 12, 3, 7]]
+]
+
+const expectedCompletions = [
+    {
+        board: [
+            [22, 13, 17, 11, 0],
+            [8, 2, 23, 4, 24],
+            [21, 9, 14, 16, 7],
+            [6, 10, 3, 18, 5],
+            [1, 12, 20, 15, 19]
+        ],
+        count: 14,
+        draw: 16,
+        markings: [
+            [false, false, true, true, true],
+            [false, true, true, true, true],
+            [true, true, true, true, true],
+            [false, true, false, false, true],
+            [false, false, false, false, false]
+        ]
+    },
+    {
+        board: [
+            [3, 15, 0, 2, 22],
+            [9, 18, 13, 17, 5],
+            [19, 8, 7, 25, 23],
+            [20, 11, 10, 24, 4],
+            [14, 21, 16, 12, 6]
+        ],
+        count: 15,
+        draw: 13,
+        markings: [
+            [false, false, true, true, false],
+            [true, false, true, true, true],
+            [false, false, true, false, true],
+            [false, true, true, true, true],
+            [true, true, true, false, false]
+        ]
+    },
+    {
+        board: [
+            [14, 21, 17, 24, 4],
+            [10, 16, 15, 9, 19],
+            [18, 8, 23, 26, 20],
+            [22, 11, 13, 6, 5],
+            [2, 0, 12, 3, 7]
+        ],
+        count: 12,
+        draw: 24,
+        markings: [
+            [true, true, true, true, true],
+            [false, false, false, true, false],
+            [false, false, true, false, false],
+            [false, true, false, false, true],
+            [true, true, false, false, true]
+        ]
+    }
+]
+
+const winBoards = [
+    [
+        [[true, true, true, true, true],
+        [false, false, false, true, false],
+        [false, false, true, false, false],
+        [false, true, false, false, true],
+        [true, true, false, false, true]],
+        0, 3
+    ],
+
+    [
+        [[true, true, false, true, true],
+        [false, false, false, true, false],
+        [false, false, true, true, false],
+        [false, true, false, true, true],
+        [true, true, false, true, true]],
+        1, 3
+    ],
+
+    [
+        [[false, false, false, true, false],
+        [false, false, false, true, false],
+        [false, false, true, false, false],
+        [false, true, false, false, true],
+        [true, true, true, true, true]],
+        4, 4
+    ],
+]
+
+const nonWinBoards = [
+    [
+        [[true, true, false, true, true],
+        [false, false, false, true, false],
+        [false, false, true, false, false],
+        [false, true, false, false, true],
+        [true, true, false, false, true]]
+    ],
+    [
+        [[true, true, false, true, true],
+        [false, false, false, true, false],
+        [false, false, true, false, false],
+        [false, true, false, true, true],
+        [true, true, false, true, true]]
+    ],
+
+    [
+        [[false, false, false, true, false],
+        [false, false, false, true, false],
+        [false, false, true, false, false],
+        [false, true, false, false, true],
+        [true, true, true, false, true]]
+    ],
+]
+
+module.exports = { boards, drawings, expectedCompletions, winBoards, nonWinBoards }
