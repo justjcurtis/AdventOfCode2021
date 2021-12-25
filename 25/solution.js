adc1c684ea6ec368602c60b7dddf31b53c371bc1
justjcurtis <justjcurtis@gmail.com>
2021-12-25 23:39:10 +0000

    feat: Day 25 solved

---
+++ 25/solution.js
@@ -0,0 +1,65 @@
+const input = require('./input')
+const test = require('./testData')
+
+const parseInput = input => input.map(line => line.split(''))
+
+const tick = grid => {
+    const w = grid[0].length
+    const h = grid.length
+        // east
+    const eastTick = grid.map(r => r.slice(0))
+    for (let j = 0; j < h; j++) {
+        for (let i = 0; i < w - 1; i++) {
+            if (grid[j][i] == '>' && grid[j][i + 1] == '.') {
+                eastTick[j][i] = '.'
+                eastTick[j][i + 1] = '>'
+                i++
+            }
+        }
+        if (grid[j][0] == '.' && grid[j][w - 1] == '>') {
+            eastTick[j][0] = '>'
+            eastTick[j][w - 1] = '.'
+        }
+    }
+
+    // south
+    const southTick = eastTick.map(r => r.slice(0))
+    for (let i = 0; i < w; i++) {
+        for (let j = 0; j < h - 1; j++) {
+            if (eastTick[j][i] == 'v' && eastTick[j + 1][i] == '.') {
+                southTick[j][i] = '.'
+                southTick[j + 1][i] = 'v'
+                j++
+            }
+        }
+        if (eastTick[0][i] == '.' && eastTick[h - 1][i] == 'v') {
+            southTick[0][i] = 'v'
+            southTick[h - 1][i] = '.'
+        }
+    }
+    return southTick
+}
+
+const gridHash = grid => grid.map(line => line.join('')).join('')
+
+const stepsToStop = grid => {
+    let a = grid.map(r => r.slice(0))
+    let steps = 1
+    while (true) {
+        let b = tick(a.map(r => r.slice(0)))
+        const aHash = gridHash(a)
+        const bHash = gridHash(b)
+        if (aHash == bHash) break
+        steps++
+        a = b.map(r => r.slice(0))
+    }
+    return steps
+}
+
+const solution = input => {
+    const grid = parseInput(input)
+    const part1 = stepsToStop(grid)
+    return { part1 }
+}
+
+console.log(solution(input))
---
+++ 25/testData.js
@@ -0,0 +1,12 @@
+const input = [
+    'v...>>.vv>',
+    '.vv>>.vv..',
+    '>>.>v>...v',
+    '>>v>>.>.v.',
+    'v>v.vv.v..',
+    '>.>>..v...',
+    '.vv..>.>v.',
+    'v.v..>>v.v',
+    '....v..v.>'
+]
+module.exports = input
