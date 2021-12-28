beb2a5dce810b7658a9d2d1a055b7f74259a7a1f
justjcurtis <justjcurtis@gmail.com>
2021-12-28 20:02:31 +0000

    perf: optimise Day 20

--- 20/solution.js
+++ 20/solution.js
@@ -1,60 +1,49 @@
-const parsePixels = pixels => parseInt(pixels.map(row => row.join('')).join('').split('').map(v => v == '.' ? '0' : '1').join(''), 2)
-
-const getPixelArray = (image, x, y, oob) => {
+const getAlgIndex = (image, x, y, oob) => {
+    const h = image.length
+    const w = image[0].length
     const pixels = [
         [{ x: x - 1, y: y - 1 }, { x, y: y - 1 }, { x: x + 1, y: y - 1 }],
         [{ x: x - 1, y }, { x, y }, { x: x + 1, y }],
         [{ x: x - 1, y: y + 1 }, { x, y: y + 1 }, { x: x + 1, y: y + 1 }],
     ]
-    let output = [
-        [],
-        [],
-        []
-    ]
+    let output = ''
     for (let j = 0; j < 3; j++) {
         for (let i = 0; i < 3; i++) {
             const { x, y } = pixels[j][i]
-            if (y < 0 || x < 0 || y >= image.length || x >= image[0].length) output[j].push(oob ? '#' : '.')
-            else output[j].push(image[y][x])
+            if (y < 0 || x < 0 || y >= h || x >= w) output += +oob
+            else output += +(image[y][x] == '#')
         }
     }
-    return output
-}
-
-const crop = image => {
-    const cropped = []
-    for (let i = 3; i < image.length - 3; i++) {
-        cropped.push(image[i].slice(3, image[i].length - 3))
-    }
-    return cropped
+    return parseInt(output, 2)
 }
 
 const applyAlg = (image, alg, oob) => {
+    const h = image.length
+    const w = image[0].length
     const output = []
-    for (let y = -4; y <= image.length + 3; y++) {
+    for (let y = -1; y <= h; y++) {
         let row = ''
-        for (let x = -4; x <= image[0].length + 3; x++) {
-            const index = parsePixels(getPixelArray(image, x, y, oob))
+        for (let x = -1; x <= w; x++) {
+            const index = getAlgIndex(image, x, y, oob)
             row += alg[index]
         }
         output.push(row)
     }
 
     oob = (oob == false ? alg[0] : alg[511]) == "#"
-    return { image: crop(output), oob }
+    return { image: output, oob }
 }
 
 const algN = (image, alg, n, oob = false) => {
-    let current = JSON.parse(JSON.stringify(image))
     for (let i = 0; i < n; i++) {
-        const result = applyAlg(current, alg, oob)
-        current = result.image
+        const result = applyAlg(image, alg, oob)
+        image = result.image
         oob = result.oob
     }
-    return { image: current, oob }
+    return { image, oob }
 }
 
-const countLit = image => image.reduce((acc, row) => [...acc, ...row], []).filter(v => v == "#").length
+const countLit = image => image.join('').split('').reduce((acc, v) => acc += +(v == '#'), 0)
 
 const solution = input => {
     const after2 = algN(input.image, input.alg, 2)
